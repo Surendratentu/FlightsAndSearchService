6816ec0ec1786d7ea1f749af797eabe31116b20f
Surendratentu <164079011+Surendratentu@users.noreply.github.com>
2025-09-09 17:18:22 +0530

    Setup Basic BodyParser

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const express =require('express');
 require('dotenv').config();
 
 const {PORT} = require('./config/serverConfig');
+const bodyParser = require('body-parser');
 
 const setupAndStartServer = async()=>{
 
     // create the express object
     const app = express();
-    const PORT = 3001;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    
 
     // create the server
     app.listen(PORT,()=>{
